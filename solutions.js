1f423a95d61bddba602a6a412f4ea0bf30820b7f
Elvis Lumnica <58716184+elumnica@users.noreply.github.com>
2020-08-06 16:11:36 -0400

    second optimal solution with best case time-space complexity

--- solutions.js
+++ solutions.js
@@ -20,3 +20,28 @@ function binarySearch(array, target) {
       
   }
   
+
+
+  // O(log(n)) time complexity | O(1) space complexity
+function binarySearch(array, target) {
+    // Write your code here.
+      return binarySearchHelper(array, target, 0, array.length - 1);
+  }
+  
+  function binarySearchHelper(array, target, left, right){
+      while (left <= right) {
+          const middle = Math.floor((left + right) / 2);
+          const potentialMatch = srray[middle];
+          if (target === potentialMatch){
+              return middle;
+          } else if (target < potentialMatch){
+              right = middle - 1;
+          } else {
+              left = middle + 1;
+          }
+      }
+      return -1;
+  }
+  // Do not edit the line below.
+  exports.binarySearch = binarySearch;
+  
